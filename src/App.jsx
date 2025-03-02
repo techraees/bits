63577106b8e4ea8e893da15f852272dffe9a1ca1
techraees <techraees786@gmail.com>
2025-03-02 12:28:39 +0500

    Update the app.jsx to remove the h1 tag

--- src/App.jsx
+++ src/App.jsx
@@ -86,7 +86,7 @@ function App() {
   return (
     <>
       <Provider store={store}>
-        <h1
+        {/* <h1
           style={{ background: "blue" }}
           onClick={async () => {
             try {
@@ -160,7 +160,7 @@ function App() {
           }}
         >
           Hello World
-        </h1>
+        </h1> */}
         <Layout />
         <ZendeskComp />
       </Provider>
