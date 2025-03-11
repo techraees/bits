8180fef8bcf392f922066b31b072521d3e453ae0
techraees <techraees786@gmail.com>
2025-03-11 15:46:11 +0500

    Implement the chnages of ownership history.

--- src/gql/queries.js
+++ src/gql/queries.js
@@ -266,8 +266,16 @@ const GET_TOP_NFTS = gql`
 
 // Used to fetch my nfts that I owned
 const Get_MY_NFTS_THAT_I_OWNED = gql`
-  query GetMyNftsThatIOwned($token: String!, $wallet_address: String!) {
-    getMyNftsThatIOwned(token: $token, wallet_address: $wallet_address)
+  query GetMyNftsThatIOwned(
+    $token: String!
+    $wallet_address: String!
+    $ownership_type: String
+  ) {
+    getMyNftsThatIOwned(
+      token: $token
+      wallet_address: $wallet_address
+      ownership_type: $ownership_type
+    )
   }
 `;
 
@@ -315,8 +323,15 @@ const GET_NFTS_THAT_I_BOUGHT = gql`
 
 //  Used to fetch the ownership history of single nfts
 const GET_OWNERSHIP_HISTORY_OF_SINGLE_NFTS = gql`
-  query GetOwnershipHistoryOfSingleNfts($_id: String!){
+  query GetOwnershipHistoryOfSingleNfts($_id: String!) {
     getOwnershipHistoryOfSingleNfts(_id: $_id)
+  }
+`;
+
+// Used to get owners who listing the same nft with different price
+const GET_OWNERS_WHO_LISTED_THE_SAME_NFT_WITH_PRICE = gql`
+  query GetOwnersWhoListedTheSameNftWithPrices($_id: String!) {
+    getOwnersWhoListedTheSameNftWithPrices(_id: $_id)
   }
 `;
 
@@ -336,6 +351,7 @@ export {
   Get_MY_NFTS_THAT_I_OWNED,
   GET_ALL_NFTS_IN_MARKET_PLACE_AND_SUPPORT_FILTER,
   GET_DETAILS_OF_SINGLE_NFT_FROM_MARKET_PLACE,
+  GET_OWNERS_WHO_LISTED_THE_SAME_NFT_WITH_PRICE,
   GET_ALL_MY_TRANSACTION,
   GET_TRANSACTION_DETAILS_OF_SPECIFIC,
   GET_NFTS_THAT_I_SOLD,
