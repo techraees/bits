f35f88ef602e53b6ec0903947e551ca97351cfd8
techraees <techraees786@gmail.com>
2025-04-29 10:36:26 -0700

    Solve the conflicts.

--- src/components/topNftsPopup/TopNftAddQuantiyPurchaseInputBodySection.jsx
+++ src/components/topNftsPopup/TopNftAddQuantiyPurchaseInputBodySection.jsx
@@ -212,37 +212,6 @@ const TopNftAddQuantiyPurchaseInputBodySection = ({
           },
         });
 
-<<<<<<< HEAD
-        const msgData = boughtMessage(
-          userData?.full_name,
-          itemData?.name,
-          itemData?.name,
-          totalcost,
-        );
-
-        await sendEmail({
-          variables: {
-            to: profileData?.GetProfileDetails?.email,
-            from: environment.REACT_APP_EMAIL_OWNER,
-            subject: msgData.subject,
-            text: msgData.message,
-          },
-        });
-
-        setLoadingStatus(false);
-        setLoadingMessage("");
-        ToastMessage("Purchase Successful", "", "success");
-        dispatch(loadContractIns());
-      } catch (error) {
-        console.error("Error in purchase", error);
-        setLoadingStatus(false);
-        const parsedEthersError = getParsedEthersError(error);
-        const errorMessage =
-          parsedEthersError.context === -32603
-            ? "Insufficient Balance"
-            : parsedEthersError.context;
-        ToastMessage("Error", errorMessage, "error");
-=======
         setLoadingStatus(false);
         setLoadingMessage("");
         ToastMessage("Purchase Successful", "", "success");
@@ -260,217 +229,216 @@ const TopNftAddQuantiyPurchaseInputBodySection = ({
     } else {
       const network = contractData?.chain == 137 ? "polygon" : "ethereum";
       ToastMessage(`Please select ${network} network`, "", "error");
->>>>>>> ae7243177436bd95a0ffd582ba9153ba5f08381f
-      }
-    };
+    }
+  };
 
-    return (
-      <>
-        {loadingStatus && <Loader content={loadingMessage} />}
-        <ConnectModal visible={connectModal} onClose={closeConnectModel} />
+  return (
+    <>
+      {loadingStatus && <Loader content={loadingMessage} />}
+      <ConnectModal visible={connectModal} onClose={closeConnectModel} />
 
-        <div className="Nunito_font_family d-flex flex-column justify-content-center align-items-center">
-          <div className="down_side_popup lg:p-4 p-3 bg-white">
-            {/* Card */}
-            <div className=" custom-card shadow-move">
-              <div className="card-body position-relative">
-                {/* Cross Button */}
-                <div className="cross_icon_red">
-                  <img
-                    onClick={() => {
-                      if (!activeButton) {
-                        settActiveButton(false);
-                        setIsFixedPriceStep(1);
-                      } else {
-                        settActiveButton(false);
-                        setIsFixedPriceStep(2);
-                      }
-                    }}
-                    src={RedCrossIcon}
-                    alt=""
-                    className="w-full h-full"
-                  />
-                </div>
+      <div className="Nunito_font_family d-flex flex-column justify-content-center align-items-center">
+        <div className="down_side_popup lg:p-4 p-3 bg-white">
+          {/* Card */}
+          <div className=" custom-card shadow-move">
+            <div className="card-body position-relative">
+              {/* Cross Button */}
+              <div className="cross_icon_red">
+                <img
+                  onClick={() => {
+                    if (!activeButton) {
+                      settActiveButton(false);
+                      setIsFixedPriceStep(1);
+                    } else {
+                      settActiveButton(false);
+                      setIsFixedPriceStep(2);
+                    }
+                  }}
+                  src={RedCrossIcon}
+                  alt=""
+                  className="w-full h-full"
+                />
+              </div>
+              <div className="">
                 <div className="">
-                  <div className="">
-                    <div
-                      className="d-flex align-items-center"
-                      style={{
-                        width: "100%",
-                      }}
-                    >
-                      <img
-                        src="https://plus.unsplash.com/premium_photo-1686727103139-2824d4fa46a3?q=80&w=3327&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                        className="rounded-circle img_quantity_top_nft mr-3"
-                        alt="Profile Image"
-                        style={{
-                          objectFit: "cover",
-                        }}
-                      />
-                      <div
-                        className="heading_data"
-                        style={{
-                          width: "90%",
-                        }}
-                      >
-                        <div className="heading_data_name">
-                          <div>{itemData?.name}</div>
-                          <div className="green_tick_button">
-                            <img
-                              src={GreenTick}
-                              alt=""
-                              className="w-full h-full"
-                            />
-                          </div>
-                        </div>
-                        <p className="">From {trimWallet(itemData?.nftOwner)}</p>
-                      </div>
-                    </div>
-
-                    <div></div>
-                  </div>
-                </div>
-
-                <div className="input_go_button_parent">
-                  {!activeButton ? (
-                    <>
-                      <div className="input_go_button_parent__input_div">
-                        <div className="input_go_button_parent__input_div__parent">
-                          <p className="input_go_button_parent__input_div__p">
-                            Select Quantity
-                          </p>
-                          <span className="input_go_button_parent__input_div__p">
-                            {quantity}
-                          </span>
-                        </div>
-
-                        <div className="increment_decrement_button">
-                          <div
-                            className="increment_decrement_button__increment"
-                            onClick={increment}
-                          >
-                            <img
-                              className="w-full"
-                              src={IncrementButtonArr}
-                              alt=""
-                            />
-                          </div>
-                          <div
-                            className="increment_decrement_button__increment"
-                            onClick={decrement}
-                          >
-                            <img
-                              className="w-full"
-                              src={DecrementButtonArr}
-                              alt=""
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </>
-                  ) : (
-                    <>
-                      <div className="input_go_button_parent__input_div">
-                        <div className="input_go_button_parent__input_div__parent_full">
-                          <p className="input_go_button_parent__input_div__p">
-                            Total Price
-                          </p>
-                          <div className="line_vertical"></div>
-                          <p>
-                            {quantity * itemData?.price}{" "}
-                            <span className="price_tag_currency">
-                              {itemData?.chainId == 137 ? "MATIC" : "ETH"} ( $
-                              {Number(
-                                quantity *
-                                itemData?.price *
-                                (itemData?.chainId == 137 ? maticBal : ethBal),
-                              ).toFixed(6)}
-                              ){" "}
-                            </span>
-                          </p>
-                        </div>
-                      </div>
-                    </>
-                  )}
-                  {!activeButton && (
-                    <div>
-                      <button
-                        className="go_button"
-                        onClick={() => {
-                          settActiveButton(true);
-                          setIsFixedPriceStep(3);
-                        }}
-                      >
-                        GO
-                      </button>
-                    </div>
-                  )}
-                </div>
-
-                {!activeButton ? (
-                  <div className="total-price mt-3">
-                    <div className="total_price_data">
-                      <p>Total Price:</p>
-                      <p>
-                        {quantity * itemData?.price}{" "}
-                        <span className="price_tag_currency">
-                          {itemData?.chainId == 137 ? "MATIC" : "ETH"} ( $
-                          {Number(
-                            quantity *
-                            itemData?.price *
-                            (itemData?.chainId == 137 ? maticBal : ethBal),
-                          ).toFixed(6)}
-                          ){" "}
-                        </span>
-                      </p>
-                    </div>
-                    <p>
-                      <span className="font-weight-bold">
-                        1 {itemData?.chainId == 137 ? "MATIC" : "ETH"}
-                      </span>{" "}
-                      =
-                      <strong>
-                        {itemData?.chainId == 137 ? maticBal : ethBal}{" "}
-                      </strong>
-                    </p>
-                  </div>
-                ) : (
                   <div
-                    className="connect_wallet_button__parent"
-                    onClick={() => {
-                      setIsFixedPriceStep(3);
-                      if (isConnected) {
-                        handlePurchase();
-                      }
-                      connectWalletHandle();
+                    className="d-flex align-items-center"
+                    style={{
+                      width: "100%",
                     }}
                   >
-                    <button className="connect_wallet_button theme_gradient_red">
-                      {isConnected ? "BuyNow" : "Connect Wallet"}
+                    <img
+                      src="https://plus.unsplash.com/premium_photo-1686727103139-2824d4fa46a3?q=80&w=3327&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                      className="rounded-circle img_quantity_top_nft mr-3"
+                      alt="Profile Image"
+                      style={{
+                        objectFit: "cover",
+                      }}
+                    />
+                    <div
+                      className="heading_data"
+                      style={{
+                        width: "90%",
+                      }}
+                    >
+                      <div className="heading_data_name">
+                        <div>{itemData?.name}</div>
+                        <div className="green_tick_button">
+                          <img
+                            src={GreenTick}
+                            alt=""
+                            className="w-full h-full"
+                          />
+                        </div>
+                      </div>
+                      <p className="">From {trimWallet(itemData?.nftOwner)}</p>
+                    </div>
+                  </div>
+
+                  <div></div>
+                </div>
+              </div>
+
+              <div className="input_go_button_parent">
+                {!activeButton ? (
+                  <>
+                    <div className="input_go_button_parent__input_div">
+                      <div className="input_go_button_parent__input_div__parent">
+                        <p className="input_go_button_parent__input_div__p">
+                          Select Quantity
+                        </p>
+                        <span className="input_go_button_parent__input_div__p">
+                          {quantity}
+                        </span>
+                      </div>
+
+                      <div className="increment_decrement_button">
+                        <div
+                          className="increment_decrement_button__increment"
+                          onClick={increment}
+                        >
+                          <img
+                            className="w-full"
+                            src={IncrementButtonArr}
+                            alt=""
+                          />
+                        </div>
+                        <div
+                          className="increment_decrement_button__increment"
+                          onClick={decrement}
+                        >
+                          <img
+                            className="w-full"
+                            src={DecrementButtonArr}
+                            alt=""
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="input_go_button_parent__input_div">
+                      <div className="input_go_button_parent__input_div__parent_full">
+                        <p className="input_go_button_parent__input_div__p">
+                          Total Price
+                        </p>
+                        <div className="line_vertical"></div>
+                        <p>
+                          {quantity * itemData?.price}{" "}
+                          <span className="price_tag_currency">
+                            {itemData?.chainId == 137 ? "MATIC" : "ETH"} ( $
+                            {Number(
+                              quantity *
+                              itemData?.price *
+                              (itemData?.chainId == 137 ? maticBal : ethBal),
+                            ).toFixed(6)}
+                            ){" "}
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                  </>
+                )}
+                {!activeButton && (
+                  <div>
+                    <button
+                      className="go_button"
+                      onClick={() => {
+                        settActiveButton(true);
+                        setIsFixedPriceStep(3);
+                      }}
+                    >
+                      GO
                     </button>
                   </div>
                 )}
               </div>
-            </div>
 
-            <div className="text-center mt-4 close_button">
-              <button
-                onClick={() => {
-                  onRequestClose();
-                  setIsFixedPriceStep(1);
-                  setIsAuctionStep(1);
-                  settActiveButton(false);
-                  setConnectModal(false);
-                }}
-                className="theme_gradient_red btn-lg close_button"
-              >
-                Close ✕
-              </button>
+              {!activeButton ? (
+                <div className="total-price mt-3">
+                  <div className="total_price_data">
+                    <p>Total Price:</p>
+                    <p>
+                      {quantity * itemData?.price}{" "}
+                      <span className="price_tag_currency">
+                        {itemData?.chainId == 137 ? "MATIC" : "ETH"} ( $
+                        {Number(
+                          quantity *
+                          itemData?.price *
+                          (itemData?.chainId == 137 ? maticBal : ethBal),
+                        ).toFixed(6)}
+                        ){" "}
+                      </span>
+                    </p>
+                  </div>
+                  <p>
+                    <span className="font-weight-bold">
+                      1 {itemData?.chainId == 137 ? "MATIC" : "ETH"}
+                    </span>{" "}
+                    =
+                    <strong>
+                      {itemData?.chainId == 137 ? maticBal : ethBal}{" "}
+                    </strong>
+                  </p>
+                </div>
+              ) : (
+                <div
+                  className="connect_wallet_button__parent"
+                  onClick={() => {
+                    setIsFixedPriceStep(3);
+                    if (isConnected) {
+                      handlePurchase();
+                    }
+                    connectWalletHandle();
+                  }}
+                >
+                  <button className="connect_wallet_button theme_gradient_red">
+                    {isConnected ? "BuyNow" : "Connect Wallet"}
+                  </button>
+                </div>
+              )}
             </div>
           </div>
-        </div>
-      </>
-    );
-  };
 
-  export default TopNftAddQuantiyPurchaseInputBodySection;
+          <div className="text-center mt-4 close_button">
+            <button
+              onClick={() => {
+                onRequestClose();
+                setIsFixedPriceStep(1);
+                setIsAuctionStep(1);
+                settActiveButton(false);
+                setConnectModal(false);
+              }}
+              className="theme_gradient_red btn-lg close_button"
+            >
+              Close ✕
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TopNftAddQuantiyPurchaseInputBodySection;
