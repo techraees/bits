0e44b4ae9bf03885cf3d1df9750b6b44f25119b7
Muhammad Usman Shamas <muhammadusman5649@gmail.com>
2025-05-17 20:46:43 +0100

    empty

--- src/utills/numberToHex.js
+++
@@ -1,6 +0,0 @@
-import { ethers } from "ethers";
-
-export const numToHex = (number) => {
-  const hex = ethers.utils.hexValue(number);
-  return hex;
-};
--- src/views/private/CreateEmote/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CreateEmote = () => {
-  return <div>Create Emote</div>;
-};
-
-export default CreateEmote;
